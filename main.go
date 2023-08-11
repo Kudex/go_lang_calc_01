dffed553bb5ea569752bd6a12c843bc44e107872
Kidron-Lisovskiy Yevhen <Kidronkudex@gmail.com>
2023-08-11 20:46:14 +0300

    ref

--- main.go
+++ main.go
@@ -10,24 +10,18 @@ import (
 )
 
 func main() {
-
 a:=app.New()
-w:= a.NewWindow("GoFoolAroundApp")
+w:= a.NewWindow("GoCalcApp")
 
 label1:=widget.NewLabel("Enter first num:")
 entry1:=widget.NewEntry()
 
 label2:=widget.NewLabel("Enter second num:")
 entry2:=widget.NewEntry()
-
-
 answer:=widget.NewLabel("")
-
-
 btn:=widget.NewButton("Count",func() {
 	n1,err :=strconv.ParseFloat(entry1.Text, 64)
 	n2, er:= strconv.ParseFloat(entry2.Text,64)
-
 	if err !=nil||er !=nil{
 answer.SetText("Error of the type of the data")
 	}else{
@@ -37,7 +31,6 @@ answer.SetText("Error of the type of the data")
 		div := n1/n2
 		answer.SetText(fmt.Sprintf("(+) %f\n(-) %f\n(*) %f\n(/) %f",sum,sub,mul,div))
 	}
-
 })
 w.SetContent(container.NewVBox(
 	label1,entry1,label2,entry2,btn,answer,
